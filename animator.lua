b7f8a5cc43d631654515aab2aefc48a0cfe5469c
Zythax <203565882+0Zythax@users.noreply.github.com>
2025-08-16 13:11:49 +0000

    Update animator.lua

--- animator.lua
+++ animator.lua
@@ -1,200 +1,171 @@
---[[
-animator.lua
-@0zythax
-cframe animation wowwww
-]]
-
-local players = game:GetService('Players');
 local tweenService = game:GetService('TweenService');
-
-local owner = players:WaitForChild('itpaw');
-local character = owner.Character or owner.CharacterAdded:Wait();
-
--- animation object
 local animation = {};
 animation.__index = animation;
 
-function animation.new(src)
+function animation.new(source)
 	local self = setmetatable({}, animation);
-
-	self.sequence = typeof(src) == 'table' and src or require(src);
-	self.useTween = true;
-
-	self.animator = nil;
-	self.tweens = {};
-	self.threads = {};
 	
-	self.looping = false;
+	self.sequence = typeof(source) == 'table' and source or require(source);
+	
 	self.playing = false;
+	self.looped = false;
 	self.animationSpeed = 1;
-	self.keyframe = 0;
-	self.lastKeyframe = 0;
-
+	self.tweeningEnabled = true;
+	
+	self.keyframe = 1;
+	self.lastKeyframe = 1;
+	self.animator = nil;
+	
 	return self;
 end
 
--- private do not call
-function animation:_cancel()
-	for _, thread in next, self.threads do
-		pcall(function()
-			task.cancel(thread);
-		end)
-	end
-
-	for _, tween in next, self.tweens do
-		tween:Cancel();
-	end
-	
-	if self.animator ~= nil then
-		self.animator.ResetJoints(self.animator);
-		self.animator.disableAnimateScript(self.animator, false);
-	end
-end
-
--- changes speed
-function animation:ChangeSpeed(newSpeed : number)
-	self.animationSpeed = newSpeed;
-end
-
--- toggles .looped
-function animation:ToggleLoop(toggle : boolean)
-	self.looping = toggle;
-end
-
--- stops
-function animation:Stop()
-	self:_cancel();
-	self = nil;
-end
-
--- plays
-function animation:Play()
-	if self.playing then return warn('already playing') end;
+function animation:resume()
+	if not self.animator then warn("No animator is handling this Animation!") end;
 	self.playing = true;
-	self.animator:PlayAnimation(self, false);
 end
 
--- pauses
-function animation:Pause()
-	if not self.playing then return warn('cannot pause a animation that isnt even playing') end
-	self:_cancel();
+function animation:pause()
+	if not self.animator then warn("No animator is handling this Animation!") end;
 	self.playing = false;
 end
 
--- animator object
+function animation:stop()
+	if not self.animator then warn("No animator is handling this Animation!") end;
+	self.animator.stopPlayingAnimations(self.animator);
+end
+
+function animation:changeSpeed(newSpeed)
+	if not self.animator then warn("No animator is handling this Animation!") end;
+	if newSpeed then
+		self.animationSpeed = math.clamp(newSpeed, 1, 5)
+	end
+end
+
+function animation:looped(toggle)
+	if not self.animator then warn("No animator is handling this Animation!") end;
+	self.looped = toggle;
+end
+
 local animator = {};
 animator.__index = animator;
 
-function animator.new(character)
+function animator.new(character : Model)
 	local self = setmetatable({}, animator);
+	
 	self.character = character;
-
+	self.humanoid = self.character:WaitForChild('Humanoid', 5);
+	
+	self.defaultC0 = {};
 	self.joints = {};
-	self.defaultc0 = {};
+	self.tweens = {};
+	
 	self.currentAnimation = nil;
-
-	self:init();
+	
+	self:initalise();
 	return self;
 end
 
--- gets stuff for the animator
-function animator:init()
+function animator:initalise()
 	for _, joint in next, self.character:GetDescendants() do
-		if joint:IsA('JointInstance') then
-			local name = joint.Part1 ~= nil and joint.Part1.Name or "";
-			self.joints[name] = joint;
-			self.defaultc0[name] = joint.C0;
+		if joint:IsA("JointInstance") and joint.Part1 then
+			local jointName = joint.Part1.Name;
+			self.joints[jointName] = joint;
+			self.defaultC0[jointName] = joint.C0;
 		end
 	end
 end
 
--- toggles the 'Animate' script
-function animator:disableAnimateScript(toggle : boolean)
-	local animate = character:FindFirstChild('Animate');
-	if animate then
-		animate.Disabled = toggle;
+function animator:resetJoints()
+	for jointName, C0 in next, self.defaultC0 do
+		self.joints[jointName].C0 = C0;
 	end
 end
 
--- play a animation
-function animator:PlayAnimation(animationObject, ...)
-	-- TODO make this not ass
-	self:disableAnimateScript(true)
-	self.currentAnimation = animationObject;
+function animator:toggleAnimateScript(toggle : boolean)
+	local animate = self.character:FindFirstChild("Animate");
+	if animate then
+		animate.Enabled = toggle;
+	end
+end
+
+function animator:playAnimation(animation)
+	if self.currentAnimation ~= nil then
+		self:stopPlayingAnimations()
+	end
+	
+	self:toggleAnimateScript(false);
+	self.currentAnimation = animation;
+	self.currentAnimation.animator = self;
 	self.currentAnimation.playing = true;
 	
-	if ... ~= false then
-		self.currentAnimation.animator = self;
-		self.currentAnimation.keyframe = 1;
-		self.currentAnimation.lastKeyframe = 1;
-	end
-	
-	local sequence = self.currentAnimation.sequence;
-	local function doAnimation()
-		for keyframe = self.currentAnimation.keyframe, #sequence do
-			table.insert(self.currentAnimation.threads, task.delay(sequence[keyframe].tm, function()
-				local actualKeyframe = sequence[self.currentAnimation.keyframe];
-				local actualLastKeyframe = sequence[self.currentAnimation.lastKeyframe]
+	self.currentAnimation.thread = task.spawn(function()
+		while self.currentAnimation.playing do
+			local actualKeyframe = self.currentAnimation.sequence[self.currentAnimation.keyframe];
+			local actualLastKeyframe = self.currentAnimation.sequence[self.currentAnimation.lastKeyframe];
+			local keyframeTime = (actualKeyframe.tm - actualLastKeyframe.tm) * self.currentAnimation.animationSpeed;
+			
+			for jointName, jointMovementData in next, actualKeyframe do
+				local joint = self.joints[jointName];
+				local defaultC0 = self.defaultC0[jointName];
 
-				for jointName, jointMovementData in next, actualKeyframe do
-					local keyframeDuration = (actualKeyframe.tm - actualLastKeyframe.tm) * (1 / animationObject.animationSpeed);
-					local joint = self.joints[jointName];
-
-					if joint ~= nil then
-						if not self.currentAnimation.useTween then
-							joint.C0 = (self.defaultc0[jointName] * jointMovementData.cf)
-						else
-							local tween = tweenService:Create(joint, 
-								TweenInfo.new(keyframeDuration, Enum.EasingStyle:FromName(jointMovementData.es), Enum.EasingDirection:FromName(jointMovementData.ed)), 
-								{['C0'] = self.defaultc0[jointName] * jointMovementData.cf}
-							);
-							table.insert(self.currentAnimation.tweens, tween);
-							tween:Play();
-						end
+				if joint and defaultC0 then
+					if not self.currentAnimation.tweeningEnabled then
+						joint.C0 = defaultC0 * jointMovementData.cf;
+					else
+						local tween = tweenService:Create(joint, TweenInfo.new(keyframeTime, Enum.EasingStyle:FromName(jointMovementData.es)), Enum.EasingDirection:FromName(jointMovementData.ed), {
+							['C0'] = defaultC0 * jointMovementData.cf;
+						});
+						table.insert(self.tweens, tween);
+						tween:Play();
 					end
 				end
-				
-				self.currentAnimation.lastKeyframe = self.currentAnimation.keyframe
-				self.currentAnimation.keyframe += 1;
-				
-				if self.currentAnimation.keyframe >= #sequence then
-					if self.currentAnimation.looping then
-						self.currentAnimation.keyframe = 1;
-						self.currentAnimation.lastKeyframe = 1;
-						doAnimation();
-						return;
-					end
-					
-					task.wait(0.02);
-					self:ResetJoints();
-					self:disableAnimateScript(false);
-				end
-			end));
+			end
+
+			task.wait(keyframeTime);
+
+			self.currentAnimation.lastKeyframe = self.currentAnimation.keyframe 
+			self.currentAnimation.keyframe = math.clamp(self.currentAnimation.keyframe + 1, 1, #self.currentAnimation.sequence);
+
+			if self.currentAnimation.keyframe == #self.currentAnimation.sequence then
+				if not self.currentAnimation.looped then break end;
+				self.currentAnimation.keyframe = 1;
+				continue;
+			end
+
+			if not self.currentAnimation.playing then
+				break;
+			end
 		end
-	end
-	
-	doAnimation();
+		
+		self:stopPlayingAnimations();
+	end)
 end
 
--- reset joints
-function animator:ResetJoints()
-	for jointName, c0 in next, self.defaultc0 do
-		self.joints[jointName].C0 = c0;
-	end
-end
-
--- stop currently playing animations (if you lose the animation object somehow Lol)
-function animator:StopPlayingAnimations()
+function animator:stopPlayingAnimations()
 	if self.currentAnimation then
-		self.currentAnimation:Stop();
-		self:ResetJoints();
-		self:disableAnimateScript(false)
+		self.currentAnimation.keyframe = 1;
+		self.currentAnimation.animationSpeed = 1;
+		self.currentAnimation.lastKeyframe = 1;
+		self.currentAnimation.playing = false;
+
+		if self.currentAnimation.thread then
+			-- yes, this errors from the thread loop but that doesn't really matter tbh cuz its terminated anyways
+			pcall(function()
+				task.cancel(self.currentAnimation.thread)
+			end)
+			self.currentAnimation.thread = nil;
+		end
+		
+		self.currentAnimation.animator = nil;
 		self.currentAnimation = nil;
 	end
-end
 
--- obligatory :Destroy function
-function animator:Destroy()
-	self:StopPlayingAnimations();
-	self = nil;
+	for _, tween in next, self.tweens do
+		pcall(function()
+			tween:Cancel();
+		end)
+	end; self.tweens = {};
+
+	self:resetJoints();
+	self:toggleAnimateScript(true);
 end
